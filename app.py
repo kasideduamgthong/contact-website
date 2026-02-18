baf9e152670ebc94908266aa1a883f0f05186654
KasidetDuangthong <ksididechdwngthxng@gmail.com>
2026-02-18 20:00:00 +0700

    Initial project rebuild

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+app.config["SECRET_KEY"] = "supersecret"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)

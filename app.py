1a6535383f37b0516218d0e3b575b0f24f5e602b
KasidetDuangthong <ksididechdwngthxng@gmail.com>
2026-02-20 09:00:00 +0700

    Add Message model

--- app.py
+++ app.py
@@ -9,6 +9,14 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
 
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+    subject = db.Column(db.String(200))
+    message = db.Column(db.Text)
+
+
 @app.route("/")
 def home():
     return render_template("home.html")

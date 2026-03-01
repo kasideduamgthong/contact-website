c76c9788aebe5dbc3d2617ccfc98c86cff9ae16d
KasidetDuangthong <ksididechdwngthxng@gmail.com>
2026-03-01 23:22:34 +0700

    Day2: Add about page

--- app.py
+++ app.py
@@ -11,5 +11,21 @@ db = SQLAlchemy(app)
 def home():
     return render_template('home.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/portfolio')
+def portfolio():
+    return render_template('portfolio.html')
+
+@app.route('/services')
+def services():
+    return render_template('services.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
